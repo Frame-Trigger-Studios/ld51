b6c06d60cf96bbb996a1b7cd364f4d24077d3017
Dylan <dylanljenkins@gmail.com>
2022-10-02 16:34:16 +1100

    sustain note generator

--- src/notes.ts
+++ src/notes.ts
@@ -1,15 +1,43 @@
 import {Entity, Scene, Sprite, SpriteConfig} from "lagom-engine";
 import {Layers} from "./LD51";
 
-const noteConfig: SpriteConfig = {
-    xOffset: -7,
-    yOffset: -7
+export enum Note {
+    LOW,
+    HIGH
+}
+
+export const getNoteSprite = (scene: Scene, note: Note): Sprite => {
+    return new Sprite(scene.game.getResource("note").texture(note, 0), {xOffset: -7, yOffset: -7});
 };
 
-export const getLowNoteSprite = (scene: Scene) => new Sprite(scene.game.getResource("note").texture(0, 0), noteConfig);
-export const getHighNoteSprite = (scene: Scene) => new Sprite(scene.game.getResource("note").texture(1, 0), noteConfig);
+export const getNoteSustainSprite = (scene: Scene, note: Note, duration: number): Sprite => {
+    return new Sprite(scene.game.getResource("note-sustain").texture(note, 0), {
+        xScale: duration,
+        xOffset: 7,
+        yOffset: -2
+    });
+};
 
-export const createNote = (bar: Entity, position: number, duration: number, note: Sprite) => {
-    const child = bar.addChild(new Entity("note1", position, 0, Layers.Notes));
-    child.addComponent(note);
+export const getNoteTailSprite = (scene: Scene, duration: number) => {
+    return new Sprite(scene.game.getResource("note-tail").texture(0, 0), {
+        xOffset: 7 + duration,
+        yOffset: -2
+    });
+};
+
+
+export const createNote = (scene: Scene, note: Note, bar: Entity, position: number, duration = 1) => {
+
+    const child = bar.addChild(new Entity("note", position, 0, Layers.Notes));
+    child.addComponent(getNoteSprite(scene, note));
+
+    // Sustains
+    if (duration === 0) {
+        throw Error("no");
+    } else if (duration === 1) {
+        // No tail
+    } else {
+        child.addComponent(getNoteSustainSprite(scene, note, duration));
+        child.addComponent(getNoteTailSprite(scene, duration));
+    }
 };
