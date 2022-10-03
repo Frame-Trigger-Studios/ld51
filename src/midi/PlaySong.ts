75b13f9d731bf459522289de62479ab324ad0b43
Peter Mandile <peterm1994@hotmail.com>
2022-10-03 21:29:57 +1100

    NOTE_SPEED is the best

--- src/midi/PlaySong.ts
+++ src/midi/PlaySong.ts
@@ -49,7 +49,7 @@ export class SongStarter extends System<[SongReady]>
             {
                 entity.addComponent(new IsPlaying());
                 entity.addComponent(new SongTime(0));
-                entity.addComponent(new Timer(4450, null, false))
+                entity.addComponent(new Timer(240 * (1000 / NOTE_SPEED), null, false))
                       .onTrigger.register(() => song.player.start(song.sequence));
             }
         });
